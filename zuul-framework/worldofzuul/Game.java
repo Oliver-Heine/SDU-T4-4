1c4134fe9767fc3698cbc94687c491d3e7b81327
Casper Andresen <casperb.andresen@gmail.com>
2020-11-03 13:15:42 +0100

    added items array, still lacking interactable array

--- zuul-framework/worldofzuul/Game.java
+++ zuul-framework/worldofzuul/Game.java
@@ -1,6 +1,9 @@
 package worldofzuul;
 
-public class Game 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Game
 {
     private Parser parser;
     private Room currentRoom;
@@ -15,12 +18,13 @@ public class Game
     private void createRooms()
     {
         Room outside, theatre, pub, lab, office;
+        Items roomItems = new Items();
       
-        outside = new Room("outside the main entrance of the university");
-        theatre = new Room("in a lecture theatre");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+        outside = new Room("outside the main entrance of the university", roomItems );
+        theatre = new Room("in a lecture theatre", roomItems);
+        pub = new Room("in the campus pub", roomItems);
+        lab = new Room("in a computing lab", roomItems);
+        office = new Room("in the computing admin office", roomItems);
         
         outside.setExit("east", theatre);
         outside.setExit("south", lab);
--- zuul-framework/worldofzuul/Item.java
+++ zuul-framework/worldofzuul/Item.java
@@ -1,9 +1,53 @@
 package worldofzuul;
 
-class Item {
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Item {
+    public String itemName;
+
+    Item(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemName (){
+        return itemName;
+    }
 
 }
 
+/*
 class InteractableItem extends Item{
-    
+
+}
+ */
+
+
+class Items{
+    public ArrayList<Item> items;
+
+    Items (ArrayList<Item> items) {
+        this.items = items;
+    }
+    Items () {
+        this(new ArrayList<Item>());
+    }
+
+    public void addItem (Item item) {
+        if (!items.contains(item)){
+            items.add(item);
+        }
+    }
+
+    public void removeItem (Item item) {
+        items.remove(item);
+    }
+
+    public String printItems() {
+        StringBuilder returnString = new StringBuilder("This room contains these items: ");
+        for (int i = 0; i<items.size() ; i++) {
+            returnString.append(", ").append(items.get(i).getItemName());
+        }
+        return returnString.toString();
+    }
 }
