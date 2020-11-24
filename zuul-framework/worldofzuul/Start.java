23542a810d55c1febe5a7d0d67eb362651ca0a13
Casper Andresen <casperb.andresen@gmail.com>
2020-11-24 11:56:04 +0100

    creating a javafx project

--- zuul-framework/worldofzuul/Start.java
+++
@@ -1,8 +0,0 @@
-package worldofzuul;
-
-public class Start {
-    public static void main(String[] args) {
-        Game g = new Game(); //create new game ojbect hej
-        g.play(); //play method on game object
-    }
-}
