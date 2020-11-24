2e914d83fd4e4b05e2c1604a04c1e26185dc060d
Daniel <danielsmidstrup1@gmail.com>
2020-11-24 12:14:21 +0100

    Simple version

--- zuul-framework/src/sample/Controller.java
+++ zuul-framework/src/sample/Controller.java
@@ -1,4 +1,39 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
 public class Controller {
+
+    @FXML
+    private ImageView kitchen;
+
+    @FXML
+    private Button computer;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void printToScreen(MouseEvent event) throws IOException {
+        System.out.println("Going to computer");
+        kitchen.setImage(new Image("sample/Image/computerRoom.jpg"));
+        computer.setVisible(false);
+        backButton.setVisible(true);
+
+    }
+
+    @FXML
+    void changeRoom(MouseEvent event) {
+        System.out.println("living!");
+        kitchen.setImage(new Image("sample/Image/bedroom.jpg"));
+        computer.setVisible(true);
+        backButton.setVisible(false);
+
+    }
 }
--- zuul-framework/src/sample/Main.java
+++ zuul-framework/src/sample/Main.java
@@ -10,12 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("room_1.fxml"));
+        Scene scene1 = new Scene(root);
+        primaryStage.setScene(scene1);
         primaryStage.show();
-    }
 
+    }
 
     public static void main(String[] args) {
         launch(args);
