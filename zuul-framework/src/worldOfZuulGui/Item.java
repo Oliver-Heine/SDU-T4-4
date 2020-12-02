518782e0322734537371ea3dd4f0106dd4ab9867
Oliver <oliverheine2002@hotmail.com>
2020-12-02 13:07:00 +0100

    Added smileys to all rooms
    Smileys was added to all rooms' progressbar

---
+++ zuul-framework/src/worldOfZuulGui/Item.java
@@ -0,0 +1,24 @@
+/*package worldOfZuulGui;
+
+public class Item {
+    public String itemName;
+    public String interactionMessage;
+    public int statusScoreChange;
+
+    public Item(String itemName, String interactionMessage, int statusScoreChange) {
+        this.itemName = itemName;
+        this.interactionMessage = interactionMessage;
+        this.statusScoreChange = statusScoreChange;
+    }
+
+    public String getItemName (){
+        return itemName;
+    }
+
+    public void itemInteraction(){
+        StatusScore.ChangeScore(statusScoreChange);
+        System.out.println(interactionMessage);
+        System.out.println("Your status score is now: " + StatusScore.getScore());
+    }
+
+}*/
