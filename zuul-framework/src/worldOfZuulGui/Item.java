53be584e9ffd923a59d1ee2b36b125632bd79a51
Daniel <danielsmidstrup1@gmail.com>
2020-12-04 22:13:27 +0100

    Items can only be interacted with once, unless you re-enter the room

--- zuul-framework/src/worldOfZuulGui/Item.java
+++ zuul-framework/src/worldOfZuulGui/Item.java
@@ -10,23 +10,31 @@ public class Item {
     private int time;
     private double scoreChange;
     private String interactionMessage;
+    private boolean canBeInteractedWith;
 
     StatusScore depressionBar = StatusScore.getInstance();
+
     public Item(int time, int scoreChange, String interactionMessage) {
         this.time = time;
         this.scoreChange = scoreChange;
+        canBeInteractedWith = true;
     }
 
     public Item(String interactionMessage, double scoreChange){
         this.scoreChange = scoreChange;
         this.interactionMessage = interactionMessage;
+        canBeInteractedWith = true;
     }
 
     public double getScoreChange(){
         return scoreChange;
     }
+
     public void changeScore(){
-        depressionBar.changeScore(scoreChange);
+        if (canBeInteractedWith){
+            depressionBar.changeScore(scoreChange);
+            canBeInteractedWith = false;
+        }
     }
 
     public String getItemInteractionMessage() {
