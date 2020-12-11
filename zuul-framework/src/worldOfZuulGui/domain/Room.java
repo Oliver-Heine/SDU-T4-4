e244d03b00f9103d8824df20b7c2ebbe3d8de9ed
Casper Andresen <casperb.andresen@gmail.com>
2020-12-11 11:06:11 +0100

    Deleted out folder and the fxml string in Room class

--- zuul-framework/src/worldOfZuulGui/domain/Room.java
+++ zuul-framework/src/worldOfZuulGui/domain/Room.java
@@ -1,31 +1,21 @@
 package worldOfZuulGui.domain;
 
 public enum Room {
-    Entry("entry.fxml"),
-    Kitchen("kitchen.fxml"),
-    LivingRoom("livingRoom.fxml"),
-    Bedroom("bedRoom.fxml"),
-    BathRoom("bathroom.fxml"),
-    Computer("computer.fxml"),
-    StartMenu("StartMenu.fxml"),
-    StartScreen("StartScreen.fxml"),
-    Tutorial("Tutorial.fxml"),
-    Tutorial_2("Tutorial_2.fxml"),
-    Tutorial_3("Tutorial_3.fxml"),
-    Tutorial_4("Tutorial_3.fxml"),
-    Tutorial_5("Tutorial_5.fxml"),
-    Tutorial_6("Tutorial_6.fxml"),
-    gameLost("gameLost.fxml"),
-    gameWon("gameWon.fxml"),
-    Fridge("fridge.fxml");
-
-    private String fxml;
-
-    Room(String fxml) {
-        this.fxml = fxml;
-    }
-
-    public String getFxml() {
-        return fxml;
-    }
+    Entry,
+    Kitchen,
+    LivingRoom,
+    Bedroom,
+    BathRoom,
+    Computer,
+    Fridge,
+    StartMenu,
+    StartScreen,
+    Tutorial,
+    Tutorial_2,
+    Tutorial_3,
+    Tutorial_4,
+    Tutorial_5,
+    Tutorial_6,
+    gameLost,
+    gameWon;
 }
